eda9d73ddedd845906918d28fccaed2bf96c3926
User <guilhermesouza-2011@hotmail.com>
2024-05-02 22:21:30 -0300

    1st

--- app/src/main/java/com/collegeapp/chatbot/JadeAgent.java
+++ app/src/main/java/com/collegeapp/chatbot/JadeAgent.java
@@ -1,11 +1,25 @@
 package com.collegeapp.chatbot;
 
+import java.util.ArrayList;
+
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 
 public class JadeAgent extends Agent
 {
     JadeManager owner;
+
+    static String[] Items = new String[]
+    {
+        "Burger",
+        "Fritas",
+        "Cola",
+        "Sorvete"
+    };
+
+    static ArrayList<String> Selected = new ArrayList<String>();
+
+    static boolean confirmingChoices = false;
 
     @Override
     protected void setup() {
@@ -26,13 +40,68 @@ public class JadeAgent extends Agent
         while (cmsg != null)
         {
             if(cmsg.equals("oi"))
-                owner.notifyBotMessage("Olá! :]");
+                owner.notifyBotMessage("Olá, atendimento do fast food aqui.");
             else if(cmsg.contains("google"))
                 owner.notifyBotMessage("Não tenho nenhuma função relacionada ao Google.");
-            else if(cmsg.contains("versão"))
-                owner.notifyBotMessage("A versão deste aplicativo é 0.0.1 Indev.");
             else if(cmsg.equals("olá"))
-                owner.notifyBotMessage("Oi! :]");
+                owner.notifyBotMessage("Oi! :] Atendimento do fast food aqui.");
+            else if(cmsg.equals("cardapio"))
+            {
+                String send = "";
+                if(Items.length > 0)
+                {
+                    for(int i = 0; i < Items.length; ++i)
+                    {
+                        send += (i + 1) +  ". " + Items[i];
+                        if(i != Items.length - 1) send += "\n";
+                    }
+                    owner.notifyBotMessage(send);
+                }
+            }
+            else if(cmsg.contains("eu quero"))
+            {
+                for(int i = 0; i < Items.length; ++i)
+                {
+                    String curr = Items[i];
+
+                    if(cmsg.contains(curr.toLowerCase()))
+                    {
+                        Selected.add(curr);
+                        owner.notifyBotMessage("Sua escolha foi adicionada.");
+                    }
+                }
+            }
+            else if(cmsg.contains("minhas compras"))
+            {
+                String send = "";
+                if(Selected.size() > 0)
+                {
+                    for(int i = 0; i < Selected.size(); ++i)
+                    {
+                        send += (i + 1) +  ". " + Selected.get(i);
+                        if(i != Selected.size() - 1) send += "\n";
+                    }
+                }
+                else send = "Nenhum items encontrado.";
+
+                owner.notifyBotMessage(send);
+            }
+            else if(cmsg.equals("comfirmar minha compra"))
+            {
+                confirmingChoices = true;
+                owner.notifyBotMessage("Você tem certeza?");
+            }
+            else if(confirmingChoices && cmsg.equals("sim"))
+            {
+                confirmingChoices = false;
+                Selected.clear();
+                owner.notifyBotMessage("Sua compra foi confirmada e esta sendo enviada para você.");
+            }
+            else if(confirmingChoices && cmsg.equals("não"))
+            {
+                confirmingChoices = false;
+                owner.notifyBotMessage("Confirmação de compra abortada.");
+            }
 
 
             cmsg = owner.popClientMessage();
